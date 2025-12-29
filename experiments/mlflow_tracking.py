3a8ec1e7ad66ea569ee8fdd4a0955b45ea335a83
Learning286 <himanshukukreja86@gmail.com>
2025-12-30 01:16:34 +0530

    mlflow_tracking edited

--- experiments/mlflow_tracking.py
+++ experiments/mlflow_tracking.py
@@ -9,7 +9,7 @@ import mlflow
 from app.config import MLFLOW_TRACKING_URI
 
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment("RAG-LLM-Experiment")
+mlflow.set_experiment("RAG-LLM-Experiment1")
 
 
 
